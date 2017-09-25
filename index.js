7c9aa5dfdd873a1955599241547a9cc315ea97ef
vaskarsarma <vaskarsarma@gmail.com>
2017-09-25 14:13:15 +0400

    test cases

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var exports = module.exports = {};
+
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
+var server = app.listen(4000, function() {
+    console.log('Magic is happening on port 4000');
+});
+
+exports.closeServer = function() {
+    server.close();
+};
---
+++ test/test.js
@@ -0,0 +1,25 @@
+var request = require("request"),
+    assert = require('assert'),
+    helloWorld = require("../index.js"),
+    base_url = "http://localhost:4000/";
+
+describe("Hello World Server", function() {
+    describe("GET /", function() {
+        it("returns status code 200", function(done) {
+            request.get(base_url, function(error, response, body) {
+                //expect(response.statusCode).toBe(200);
+                assert.equal(200, response.statusCode);
+                done();
+            });
+        });
+
+        it("returns Hello World", function(done) {
+            request.get(base_url, function(error, response, body) {
+                //expect(body).toBe("Hello World");
+                assert.equal("Hello World", body);
+                helloWorld.closeServer();
+                done();
+            });
+        });
+    });
+});
