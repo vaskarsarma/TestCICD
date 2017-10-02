e7a6f32a9d6cecb5ee4ee0270866019b20c213d8
vaskarsarma <vaskarsarma@gmail.com>
2017-10-02 15:51:22 +0400

    test case 16

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var exports = module.exports = {};
 
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.send('Hello');
 });
 
 var server = app.listen(4000, function() {
